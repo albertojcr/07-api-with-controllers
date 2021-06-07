cd26402e957325600192b15997601b7899fca089
albertojcr <albertojcr96@gmail.com>
2021-06-07 13:16:10 +0200

    Edit article in progress2

--- src/Application/Article/EditArticle/EditArticleService.php
+++ src/Application/Article/EditArticle/EditArticleService.php
@@ -15,7 +15,8 @@ class EditArticleService
         $this->articleRepository = $articleRepository;
     }
 
-    public function execute(string $id, string $name, string $description, string $image, bool $isActive, string $endDate, int $currentPrice, int $directBidPrice1, int $directBidPrice2, int $directBidPrice3)
+    public function execute(string $id, string $name, string $description, string $image, bool $isActive, string $endDate,
+                            int $currentPrice, int $directBidPrice1, int $directBidPrice2, int $directBidPrice3)
     {
         $article = Article::edit($id, $name, $description, $image, $isActive, $endDate, $currentPrice, $directBidPrice1, $directBidPrice2, $directBidPrice3);
         $this->articleRepository->edit($article);
